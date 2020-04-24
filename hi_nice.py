7227a167f6cb650c783a8e01bf69625522217732
Rupert <47789156+dublowduck@users.noreply.github.com>
2020-04-24 22:12:16 +0100

    Renamed hi.py
    Renamed hi.py to hi_cat.py so I can use it. Added hi.py which is the same as hi_cat.py but without the swearing, so I can demonstrate to grown-ups.

--- hi_nice.py
+++ hi_nice.py
@@ -1,10 +1,9 @@
 import time
-name = "cat"
 print("Hi")
 time.sleep(1.5)
 print("What`s your name?")
-name = str(input())
+name = input()
 time.sleep(0.5)
 print("Hello " + name)
 time.sleep(2)
-print("Now fuck off")
+print("Now go away")
